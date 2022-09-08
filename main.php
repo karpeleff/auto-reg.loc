cb8a36800a08ea418b81320392d0fd4c4dd7a138
Aleksey <alekseykarpelev@gmail.com>
2022-09-08 14:08:51 +1000

    add sert

--- main.php
+++ main.php
@@ -1,6 +1,15 @@
 <?php
 class main
 {
+    protected  $url = 'https://dash.cloudflare.com/sign-up';
+
+    function __construct()
+    {
+
+    }
+
+
+
   public  function  index()
   {
       $ch = curl_init();
@@ -17,7 +26,33 @@ class main
 
   public function test()
   {
-      echo 'i am test';
+
+      $url = 'https://dash.cloudflare.com/sign-up';
+      $options = array(
+          CURLOPT_RETURNTRANSFER => true,     // return web page
+          CURLOPT_HEADER         => false,    // don't return headers
+          CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+          CURLOPT_ENCODING       => "",       // handle all encodings
+          CURLOPT_USERAGENT      => "spider", // who am i
+          CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+          CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+          CURLOPT_TIMEOUT        => 120,      // timeout on response
+          CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+          CURLOPT_CAINFO         => 'cacert.pem',
+      );
+
+      $ch      = curl_init( $url );
+      curl_setopt_array( $ch, $options );
+      $content = curl_exec( $ch );
+      $err     = curl_errno( $ch );
+      $errmsg  = curl_error( $ch );
+      $header  = curl_getinfo( $ch );
+      curl_close( $ch );
+
+      $header['errno'] = $err;
+      $header['errmsg'] = $errmsg;
+      $header['content'] = $content;
+      return $header;
   }
 
 
