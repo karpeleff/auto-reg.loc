3b99fd98bfdaf88cb8c8386b389dfdfa47630c44
Aleksey <alekseykarpelev@gmail.com>
2022-09-08 13:52:28 +1000

    add index

--- main.php
+++ main.php
@@ -1,1 +1,12 @@
 <?php
+
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://dash.cloudflare.com/sign-up");
+
+//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+//Выполняем запрос
+curl_exec($ch);
+//Завершает сеанс
+curl_close($ch);
